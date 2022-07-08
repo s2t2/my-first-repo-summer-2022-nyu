b87acf3488384e2b5a1dde6532eaadc9e19a67c6
MJ Rossetti <s2t2@users.noreply.github.com>
2022-07-07 21:37:40 -0400

    Create cheat_sheet.py

---
+++ cheat_sheet.py
@@ -0,0 +1,28 @@
+
+# calculations
+
+# printing
+
+print(2+2)
+
+print(3+3)
+
+100
+
+
+# variables
+
+# variables
+
+
+x = 100
+
+y = 50
+
+print(x + y)
+
+
+
+
+x = input("Please choose a fav ice cream flavor: ")
+print(x)
