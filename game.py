5ab56962fa034d9dd1f19c0703509069bb7b6d5d
MJ Rossetti <s2t2@users.noreply.github.com>
2022-07-12 19:53:38 -0400

    User choice

--- game.py
+++ game.py
@@ -7,6 +7,12 @@ print("WELCOME TO ROCK PAPER SCISSORS GAME")
 # USER INPUTS
 
 
+user_choice = input("Please make a selection ('rock', 'paper', 'scissors'): ")
+
+# You chose: 'rock'
+print("You chose:", user_choice) # SUFFICIENT!!
+print(f"You chose: '{user_choice}' ")
+
 # VALIDATE USER INPUTS
 
 
